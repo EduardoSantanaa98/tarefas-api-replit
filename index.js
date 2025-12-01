a062ed960796e6818ff6df4e49b7d35482296086
EduardoSantanaa98 <luiz.eduardo04@aluno.ifce.edu.br>
2025-12-01 00:32:37 +0000

    Primeiro commit - Lista de tarefas

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 5000;
 
-// Lista de tarefas
+
 let tarefas = [];
 
-// Configurar EJS
+
 app.set("view engine", "ejs");
 
-// Habilitar body-parser para receber POST
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Rota principal -> renderiza o formulário
+
 app.get("/", (req, res) => {
   res.render("index", { tarefas });
 });
 
-// Rota para adicionar tarefa
+
 app.post("/add", (req, res) => {
   const titulo = req.body.titulo;
 
